902afa49730de7ff4396a2743c57390a1cec4997
Ahmad Kaif <heyahmad611@gmail.com>
2025-04-09 23:33:16 +0530

    analyzing throgh TextBlob

---
+++ Readme.md
@@ -0,0 +1,31 @@
+
+### 1. Create a virtual env
+```
+ python3 -m venv venv
+```
+
+### 2. Activate the env
+```
+ source venv/bin/activate
+
+```
+
+### 3. Down load all the dependencies
+```
+pip install -r requirements.txt
+```
+### 4. Run
+```
+python3 app.py
+```
+---
+---
+---
+---
+---
+
+
+### * For developers *
+```
+pip freeze > requirements.txt
+```
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from textblob import TextBlob
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/analyze", methods=["POST"])
+def analyze_sentiment():
+    data = request.json
+    content = data.get("text", "")
+    
+    blob = TextBlob(content)
+    polarity = blob.sentiment.polarity
+
+    return jsonify({ "polarity": polarity })
+
+if __name__ == "__main__":
+    app.run(port=5001)
